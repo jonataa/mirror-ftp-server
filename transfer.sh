b812a6f597494e26e421fda4b622e34ecf1db26e
Jonata Weber <jonataa@gmail.com>
2019-07-20 19:32:49 -0300

    Upload all files to remote using LFTP

--- transfer.sh
+++ transfer.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 LFTP_COMMAND="docker run -it --network=host -v $PWD:/tmp lftp" 
 TARGET_LOCAL=./tmp
 
@@ -6,7 +6,14 @@ download_files_from_ftp()
 {
   REMOTE_FOLDER=$1
   TARGET_FOLDER=$2
-  $LFTP_COMMAND -e "mirror --target-director=$TARGET_FOLDER;exit" $REMOTE_FOLDER
+  $LFTP_COMMAND -e "mirror --target-directory=$TARGET_FOLDER;exit" $REMOTE_FOLDER
+}
+
+upload_files_to_ftp()
+{
+  REMOTE_FOLDER=$1
+  SOURCE_FOLDER=$2
+  $LFTP_COMMAND -e "lcd $SOURCE_FOLDER;mirror -R;exit" $REMOTE_FOLDER
 }
 
 clean()
@@ -16,6 +23,18 @@ clean()
   mkdir $TMP_FOLDER
 }
 
+NC='\033[0m'
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+
+echo "${YELLOW}Clean Up...${NC}"
 clean $TARGET_LOCAL
+
+echo "${BLUE}Download all files from $1${NC}"
 download_files_from_ftp $1 $TARGET_LOCAL
 
+echo "${BLUE}Upaload all files to $2${NC}"
+upload_files_to_ftp $2 $TARGET_LOCAL
+
+echo "🍺 ${GREEN}Finished.${NC}"
